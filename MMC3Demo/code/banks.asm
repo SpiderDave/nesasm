00be2fd25d64ffd4bd28d93a7f34080a59ed3e2b
SpiderDave <ifallelsefailsusefire@gmail.com>
2021-04-16 06:48:01 -0400

    removing () from lda (messages_lo),y and lda (messages_hi),y even though asm6 allows it.

--- MMC3Demo/code/banks.asm
+++ MMC3Demo/code/banks.asm
@@ -1,20 +1,24 @@
 ; bank 0 
+;bank 0
 .base $8000
 .db $00                     ; bank identifier
 .include code\bank00.asm
 .pad $c000
 
+;bank 1
 ; bank 1
 .base $8000
 .db $01                     ; bank identifier
 .include code\bank01.asm
 .pad $c000
 
+;bank 2
 ; bank 2
 .base $8000
 .db $02                     ; bank identifier
 .include code\bank02.asm
 .pad $c000
 
+;bank 3
 ; fixed bank
 .org $c000
